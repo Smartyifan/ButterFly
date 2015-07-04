0234ef1a4eeb9458c4ebebab6ee603f636ba12f2
Smartyifan <Smartyifan@outlook.com>
2015-07-04 20:16:39 +0800

    a blank project.

---
+++ User/main.c
@@ -0,0 +1,54 @@
+/**
+  ******************************************************************************
+  * @file    STM32工程/工程模版/User
+  * @author  贾一帆
+  * @version V3.5.0
+  * @date    2014-10-4
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  * No attention
+  ******************************************************************************
+  */  
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+
+
+/** @addtogroup STM32F10x_StdPeriph_Template
+  * @{
+  */
+#include "sys/sys.h"  
+#include "delay/delay.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  *@brief   Initial
+  *@param   None;
+  *@retval  None
+  */
+void Initial()
+{
+    
+}
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+    Initial();
+    while(1)
+    {
+        
+    }
+}
+
+/******************* (C) COPYRIGHT 2014 STMicroelectronics *****END OF FILE****/
